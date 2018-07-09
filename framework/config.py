ff842b38c0e018a4e6684debc6f6d0c7f033f4c4
Sulom <sulom.tulshibagwale@izeltech.com>
2018-07-09 12:35:36 +0530

    Added Config file details

--- framework/config.py
+++ framework/config.py
@@ -1,4 +1,13 @@
-"""
+import requests
+import json
+import datetime
+import threading
+import time
 
-D
-"""
+instance_id = "i-0380596e10d91fb42"
+CVM_IP = "18.130.59.250"
+Region = "eu-west-2"
+SecretKey = "9Z1vn11Buts9vS9y+0hWfxCNyWvyUrcREtCQsPkG"
+AccessKey = "AKIAJ3ELMVYRLCJQGHRQ"
+
+
